0807deded97229e0918ac350e85fe3c3121fb044
andbar-ru <wind29121982@gmail.com>
2019-05-03 20:10:36 +0300

    Adjust to the new site disign

--- bingwallpaper.go
+++ bingwallpaper.go
@@ -1,8 +1,8 @@
 /*
 Script downloads today's wallpaper from bingwallpaper.com, sets wallpaper and shows message with
-wallpaper description. Information about downloaded wallpapers is saved into WP_FILE. If today's
+wallpaper description. Information about downloaded wallpapers is saved into wpFile. If today's
 wallpaper has been downloaded already, script does nothing. If there are missed dates, script
-downloads wallpapers at that dates. WP_FILE's lines have the following format:
+downloads wallpapers at that dates. wpFile's lines have the following format:
 YYYYMMDD <wallpaper-file-name> <description>.
 */
 package main
@@ -22,17 +22,18 @@ import (
 )
 
 const (
-	BASE_URL           = "https://bing.gifposter.com"
-	LOCAL_DATE_LAYOUT  = "20060102"
-	REMOTE_DATE_LAYOUT = "Jan 2, 2006"
+	baseURL          = "https://bing.gifposter.com"
+	startURL         = "https://bing.gifposter.com/list/new/desc/classic.html"
+	localDateLayout  = "20060102"
+	remoteDateLayout = "Jan 2, 2006"
 )
 
 var (
-	IMG_DIR   = fmt.Sprintf("%s/Images/bing-wallpapers", os.Getenv("HOME"))
-	WP_FILE   = fmt.Sprintf("%s/wallpapers", IMG_DIR)
+	imgDir    = fmt.Sprintf("%s/Images/bing-wallpapers", os.Getenv("HOME"))
+	wpFile    = fmt.Sprintf("%s/wallpapers", imgDir)
 	now       = time.Now()
-	TODAY     = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
-	YESTERDAY = TODAY.AddDate(0, 0, -1)
+	today     = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+	yesterday = today.AddDate(0, 0, -1)
 	lastDate  time.Time
 )
 
@@ -42,49 +43,62 @@ func check(err error) {
 	}
 }
 
+func getResponse(url string) *http.Response {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Panicf("Could not get response from url %s", url)
+	}
+	if response.StatusCode != 200 {
+		log.Panicf("%s: status code error: %d %s", url, response.StatusCode, response.Status)
+	}
+	return response
+}
+
 // Download wallpaper from the url.
 func downloadWallpaper(url string) (time.Time, string, string) {
 	var date time.Time
 	var filename, description string
 
-	// Fetch the page with photo
-	response, err := http.Get(url)
-	check(err)
+	// Page with photo metadata.
+	response := getResponse(url)
 	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		log.Panicf("%s: status code error: %d %s", url, response.StatusCode, response.Status)
-	}
-
-	// Parse the page and fetch the image metadata
 	root, err := goquery.NewDocumentFromReader(response.Body)
 	check(err)
-	detail := root.Find(".detail")
-	dateStr := detail.Find("time[itemprop='date']").Text()
-	date, err = time.Parse(REMOTE_DATE_LAYOUT, dateStr)
-	check(err)
 
-	description = detail.Find(".description").Text()
+	// Parse the page and fetch the image metadata and href.
+	solo := root.Find("section.solo")
+	dateStr := solo.Find("time").Text()
+	date, err = time.Parse(remoteDateLayout, dateStr)
+	check(err)
+	description = solo.Find("article.fl p").Text()
+	href, ok := solo.Find("a.fl").First().Attr("href")
+	if !ok {
+		log.Panicf("Could not find solo href at date %s", date.Format(localDateLayout))
+	}
+	href = baseURL + href
+
+	// Page with photo.
+	response = getResponse(href)
+	defer response.Body.Close()
+	root, err = goquery.NewDocumentFromReader(response.Body)
+	check(err)
 
 	img := root.Find("#bing_wallpaper")
 	src, ok := img.Attr("src")
 	if !ok {
 		log.Panicf("Could not find img src on url %s", url)
 	}
-	src = BASE_URL + src
 	lastSlashIndex := strings.LastIndex(src, "/")
 	filename = src[lastSlashIndex+1:]
-	filepath := fmt.Sprintf("%s/%s", IMG_DIR, filename)
+	filepath := fmt.Sprintf("%s/%s", imgDir, filename)
 
-	// Download image
+	// Download image.
 	output, err := os.Create(filepath)
 	if err != nil {
 		log.Panicf("Could not create file %s, err: %s", filepath, err)
 	}
 	defer output.Close()
-	response, err = http.Get(src)
-	if err != nil {
-		log.Panicf("Could not download image from %s, err: %s", src, err)
-	}
+	response = getResponse(src)
 	defer response.Body.Close()
 	_, err = io.Copy(output, response.Body)
 	if err != nil {
@@ -95,32 +109,33 @@ func downloadWallpaper(url string) (time.Time, string, string) {
 }
 
 // Set wallpaper and show message with description.
-func setWallpaper(filename, description string) {
-	filepath := fmt.Sprintf("%s/%s", IMG_DIR, filename)
+func setWallpaper(filename, title, description string) {
+	filepath := fmt.Sprintf("%s/%s", imgDir, filename)
 
 	setWallpaperCmd := exec.Command("fbsetbg", "-f", filepath)
 	err := setWallpaperCmd.Start()
 	check(err)
 
-	msgCmd := exec.Command("zenity", "--info", "--width=600", "--no-markup", "--text", description)
+	msgCmd := exec.Command("zenity", "--info", "--width=600", "--no-markup", "--title", title, "--text", title+".\n\n"+description)
 	err = msgCmd.Start()
 	check(err)
 }
 
 // Save record about wallpaper into file.
-func logWallpaper(date time.Time, filename, description string) {
+func logWallpaper(date time.Time, filename, title, description string) {
 	// Escape some characters for sed.
+	description = title + ".  " + description
 	fixedDescription := description
 	fixedDescription = strings.Replace(fixedDescription, "&", `\x26`, -1)
 	fixedDescription = strings.Replace(fixedDescription, "'", `\x27`, -1)
 	fixedDescription = strings.Replace(fixedDescription, ";", `\x3b`, -1)
-	line := fmt.Sprintf("%s %s %s\\n", date.Format(LOCAL_DATE_LAYOUT), filename, fixedDescription)
-	sedCmd := exec.Command("sed", "-i", fmt.Sprintf("1s;^;%s;", line), WP_FILE)
+	line := fmt.Sprintf("%s %s %s\\n", date.Format(localDateLayout), filename, fixedDescription)
+	sedCmd := exec.Command("sed", "-i", fmt.Sprintf("1s;^;%s;", line), wpFile)
 	err := sedCmd.Run()
 	check(err)
 
 	// Check that first line matches original description.
-	f, err := os.Open(WP_FILE)
+	f, err := os.Open(wpFile)
 	check(err)
 	defer f.Close()
 	reader := bufio.NewReader(f)
@@ -129,91 +144,96 @@ func logWallpaper(date time.Time, filename, description string) {
 	substrings := strings.SplitN(firstLine, " ", 3)
 	savedDescription := substrings[len(substrings)-1]
 	if strings.TrimSpace(savedDescription) != strings.TrimSpace(description) {
-		log.Printf("%s: Original description and saved description are mismatched.", date.Format(LOCAL_DATE_LAYOUT))
+		log.Printf("%s: Original description and saved description are mismatched.", date.Format(localDateLayout))
 	}
 }
 
 func main() {
-	// Create directory if not exists
-	_, err := os.Stat(IMG_DIR)
+	// Create directory if not exists.
+	_, err := os.Stat(imgDir)
 	if os.IsNotExist(err) {
-		err = os.Mkdir(IMG_DIR, 0755)
+		err = os.Mkdir(imgDir, 0755)
 		check(err)
 	}
 
 	// Fetch the last date and, if the last date is today, exit
-	_, err = os.Stat(WP_FILE)
+	_, err = os.Stat(wpFile)
 	if os.IsNotExist(err) {
-		f, err := os.Create(WP_FILE)
+		f, err := os.Create(wpFile)
 		check(err)
 		_, err = f.WriteString("\n")
 		check(err)
 		f.Close()
 	} else {
-		f, err := os.Open(WP_FILE)
+		f, err := os.Open(wpFile)
 		check(err)
 		lastDateBytes := make([]byte, 8) // YYYYMMDD
 		_, err = f.Read(lastDateBytes)
 		check(err)
 		f.Close()
 
-		lastDate, err = time.Parse(LOCAL_DATE_LAYOUT, string(lastDateBytes))
+		lastDate, err = time.Parse(localDateLayout, string(lastDateBytes))
 		check(err)
-		if lastDate == TODAY {
+		if lastDate == today {
 			os.Exit(0)
 		}
 	}
 	if lastDate.IsZero() {
-		lastDate = YESTERDAY
+		lastDate = yesterday
 	}
 
 	// Page with thumbs.
-	response, err := http.Get(BASE_URL)
-	check(err)
+	response := getResponse(startURL)
 	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		log.Panicf("status code error: %d %s", response.StatusCode, response.Status)
-	}
 	root, err := goquery.NewDocumentFromReader(response.Body)
 	check(err)
-	thumbs := root.Find("article.thumb")
+
+	thumbs := root.Find("ul.imglist > li")
 	if thumbs.Length() == 0 {
 		log.Panicf("Could not find thumbs")
 	}
 
-	// Collect urls until the last date
+	// Collect urls and titles until the last date.
 	urls := make([]string, 0)
+	titles := make([]string, 0)
 	thumbs.EachWithBreak(func(i int, thumb *goquery.Selection) bool {
-		dateStr := thumb.Find("time.date").First().Text()
-		date, err := time.Parse(REMOTE_DATE_LAYOUT, dateStr)
+		dateStr := thumb.Find("time").First().Text()
+		date, err := time.Parse(remoteDateLayout, dateStr)
 		check(err)
 
 		if !date.After(lastDate) {
 			return false
 		}
 		// Tomorrow date may exist but attempt to download wallpaper returns error 404.
-		if date.After(TODAY) {
+		if date.After(today) {
 			return true
 		}
 
 		href, ok := thumb.Find("a").First().Attr("href")
 		if !ok {
-			log.Panicf("Could not find url at date %s", date.Format(LOCAL_DATE_LAYOUT))
+			log.Panicf("Could not find url at date %s", date.Format(localDateLayout))
 		}
-		url := BASE_URL + href
+		url := baseURL + href
 		urls = append(urls, url)
+
+		title := thumb.Find("span").First().Text()
+		if title == "" {
+			log.Panicf("Thumb without a title at date %s?", date.Format(localDateLayout))
+		}
+		titles = append(titles, title)
+
 		return true
 	})
 
-	// If there are new urls, range them from the end until the first: only download and log.
+	// If there are new urls, range them from last to first exclusive: only download and log.
 	if len(urls) > 0 {
 		for i := len(urls) - 1; i > 0; i-- {
 			date, filename, description := downloadWallpaper(urls[i])
-			logWallpaper(date, filename, description)
+			logWallpaper(date, filename, titles[i], description)
 		}
 		// For the first url further set wallpaper and output message.
 		date, filename, description := downloadWallpaper(urls[0])
-		setWallpaper(filename, description)
-		logWallpaper(date, filename, description)
+		setWallpaper(filename, titles[0], description)
+		logWallpaper(date, filename, titles[0], description)
 	}
 }
